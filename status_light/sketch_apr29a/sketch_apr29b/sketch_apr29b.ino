c6e7ba32d38b366d6a8462060172a15f51096533
Nicholas.Zheng <leglars@gmail.com>
2016-05-05 16:09:02 +1000

    testing sin curve

--- status_light/sketch_apr29a/sketch_apr29b/sketch_apr29b.ino
+++ status_light/sketch_apr29a/sketch_apr29b/sketch_apr29b.ino
@@ -5,22 +5,30 @@
         CRGB leds[NUM_LEDS];
         void setup() { FastLED.addLeds<NEOPIXEL,PIN>(leds, 16); }
         void loop() { 
-//                for(int dot = 0; dot < 255*6; dot++) { 
-//                  if( (dot/255)%2 == 0) {
-//                    leds[1].setHSV(235, 162, dot);
-//                    FastLED.show();
-//                    }
-//                    else {
-//                      leds[1].setHSV(235, 162, 255-(dot%255));
-//                      FastLED.show();
-//                      }
-//            
-//            // clear this led for the next time around the loop
-//        }
-        leds[1].setHSV(235,231,200);
-        FastLED.show();
-        delay(1000);
-        leds[1].fadeToBlackBy(64);
-        FastLED.show();
-        delay(2000);
+                for(int dot = 0; dot < 511; dot++) { 
+                  if( dot < 255) {
+                    for(int i =0; i < 16; i++) {
+                    leds[i].setHSV(235, 162, dot);
+                    
+                        }
+                        FastLED.show();
+                  }
+                    else {
+                      for(int i =0; i < 16; i++) {
+                      leds[i].setHSV(235, 162, 511 - dot);
+
+                      }
+                    FastLED.show();
+               
+                      
+                    }
+            
+            // clear this led for the next time around the loop
+        }
+//        leds[1].setHSV(235,231,200);
+//        FastLED.show();
+//        delay(1000);
+//        leds[1].fadeToBlackBy(64);
+//        FastLED.show();
+//        delay(2000);
         }
