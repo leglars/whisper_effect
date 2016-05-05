0945accec0606072a29de823e0f55c752b90e126
Jianghui Zhang <xiaoanetsuko@hotmail.com>
2016-05-05 16:09:24 +1000

    Added distance sensor folder

---
+++ distance/distance.ino
@@ -0,0 +1,50 @@
+/*
+ HC-SR04 Ping distance sensor]
+ VCC to arduino 5v GND to arduino GND
+ Echo to Arduino pin 13 Trig to Arduino pin 12
+ Red POS to Arduino pin 11
+ Green POS to Arduino pin 10
+ 560 ohm resistor to both LED NEG and GRD power rail
+ More info at: http://goo.gl/kJ8Gl
+ Original code improvements to the Ping sketch sourced from Trollmaker.com
+ Some code and wiring inspired by http://en.wikiversity.org/wiki/User:Dstaub/robotcar
+ */
+
+#define trigPin 13
+#define echoPin 12
+#define relay 11
+#define led 10
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+  pinMode(relay, OUTPUT);
+}
+
+void loop() {
+  long duration, dist_relay, dist_led;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+//  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  dist_relay = (duration/2) / 29.1;
+  dist_led = (duration/2) / 29.1;
+  if (dist_led < 30) {  // This is where the LED On/Off happens
+    digitalWrite(led,HIGH); // When the Red condition is met, the Green LED should turn off
+}
+  else {
+    digitalWrite(led,LOW);
+  }
+  if (dist_relay < 6) {
+    digitalWrite(relay, HIGH);
+  }
+    else{
+      digitalWrite(relay, LOW);
+    }
+  delay(500);
+}
