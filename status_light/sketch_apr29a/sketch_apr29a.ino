e384a278bda35b5d347f5e87f25a62ef75345c2f
leglars.Zheng <leglars@gmail.com>
2016-05-12 00:17:25 +1000

    update the new light pattern -- breathing

--- status_light/sketch_apr29a/sketch_apr29a.ino
+++ status_light/sketch_apr29a/sketch_apr29a.ino
@@ -1,3 +1,5 @@
+#include <Arduino.h>
+
 
 #include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
--- status_light/sketch_apr29a/sketch_apr29b/sketch_apr29b.ino
+++ status_light/sketch_apr29a/sketch_apr29b/sketch_apr29b.ino
@@ -1,34 +1,37 @@
-   #include "FastLED.h"
-  #define NUM_LEDS 16
-  #define PIN 6
-  
-        CRGB leds[NUM_LEDS];
-        void setup() { FastLED.addLeds<NEOPIXEL,PIN>(leds, 16); }
-        void loop() { 
-                for(int dot = 0; dot < 511; dot++) { 
-                  if( dot < 255) {
-                    for(int i =0; i < 16; i++) {
-                    leds[i].setHSV(235, 162, dot);
-                    
-                        }
-                        FastLED.show();
-                  }
-                    else {
-                      for(int i =0; i < 16; i++) {
-                      leds[i].setHSV(235, 162, 511 - dot);
+#include <Arduino.h>
 
-                      }
-                    FastLED.show();
-               
-                      
-                    }
-            
-            // clear this led for the next time around the loop
+#include "FastLED.h"
+#define NUM_LEDS 16
+#define PIN 6
+  
+CRGB leds[NUM_LEDS];
+void setup() {Serial.begin(9600); FastLED.addLeds<NEOPIXEL,PIN>(leds, 16); }
+void loop() { 
+  breathPattern();
+  Serial.println();
+  }
+    
+void linearPattern() {
+  for(int dot = 0; dot < 511; dot++) { 
+    if( dot < 255) {
+      for(int i =0; i < 16; i++) {
+      leds[i].setHSV(235, 162, dot); 
+      }
+    FastLED.show();
+    }else {
+        for(int i =0; i < 16; i++) {
+        leds[i].setHSV(235, 162, 511 - dot);
         }
-//        leds[1].setHSV(235,231,200);
-//        FastLED.show();
-//        delay(1000);
-//        leds[1].fadeToBlackBy(64);
-//        FastLED.show();
-//        delay(2000);
-        }
+    FastLED.show();
+    }
+  }
+}
+
+void breathPattern() {
+  for(int dot = 0; dot < (256 * 4) - 1; dot++) {
+    for(int i = 0; i < 16; i++) {
+      leds[i].setHSV(235, 162, quadwave8(dot/4));
+    }
+    FastLED.show();
+  }
+}
