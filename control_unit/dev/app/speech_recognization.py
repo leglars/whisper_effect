89fd3e1577347f37f9f5bddaa38e8d6058d78ff8
leglars <leglars@gmail.com>
2016-05-21 12:22:50 +1000

    loop until confirm

--- control_unit/dev/app/speech_recognization.py
+++ control_unit/dev/app/speech_recognization.py
@@ -4,7 +4,7 @@ m = sr.Microphone.list_microphone_names()
 print(m)
 
 r = sr.Recognizer()
-r.energy_threshold = 300
+r.energy_threshold = 1000
 r.pause_threshold = 0.5
 
 CONFIRM = ["yes", "ready"]
@@ -14,16 +14,27 @@ def print_phrase(audio_data):
     text = r.recognize_google(audio_data)
     print(text)
 
-# 'Built-in Microph' is the name of christine's headphone in Mac mini
-with sr.Microphone(device_index=1, sample_rate=16000, chunk_size=1024) as source:
-    while True:
-        print("looping")
-        audio_data = r.listen(source, )  # return an audio_data
-        transcription = r.recognize_google(audio_data)
 
-        print(transcription)
+def listen():
+    # 'Built-in Microph' is the name of christine's headphone in Mac mini
+    with sr.Microphone(device_index=1, sample_rate=16000, chunk_size=1024) as source:
+        while True:
+            print("looping")
+            audio_data = r.listen(source)  # return an audio_data
+            try:
+                # transcription = r.recognize_google(audio_data)
+                transcription = r.recognize_sphinx(audio_data)
+            except sr.UnknownValueError:
+                print("no word understand")
+                continue
 
-        if transcription in CONFIRM:
-            break
-    # r.listen_in_background(source, print_phrase)
+            print(transcription)
 
+            for word in transcription.split(' '):
+                if word in CONFIRM:
+                    return True
+            # r.listen_in_background(source, print_phrase)
+
+
+if listen():
+    print("haha")
