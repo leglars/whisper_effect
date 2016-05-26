0e734117fe80115ac61f8c15ce8eff7a8cf31c9f
leglars.Zheng <leglars@gmail.com>
2016-05-26 12:17:45 +1000

    ping to get dist

--- control_unit/arduino_connection.py
+++ control_unit/arduino_connection.py
@@ -1,4 +1,5 @@
 from time import sleep
+
 import serial
 
 
@@ -6,7 +7,7 @@ class arduinoConnection(object):
 
     def __init__(self):
         self.dist_port = 'COM8' # '/dev/cu.usbmodem1461'
-        self.dist_freq = 19200
+        self.dist_freq = 9600
 
         self.light_port = 'COM4' # '/dev/cu.usbmodem1461'
         self.light_freq = 9600
@@ -20,11 +21,10 @@ class arduinoConnection(object):
     def read_dist(self):
         while True:
             d = self.get_dist()
-
+            print(d)
             if 6 < d < 30:
                 if not self.__working_flag:
                     self.change2working()
-                    break
             elif d <= 6:
                 if not self.__processing_flag:
                     self.change2processing()
@@ -35,11 +35,43 @@ class arduinoConnection(object):
         # the unit of d is cm
         return int(str(distance).strip().split("\'")[1].split("\\r")[0])
 
+    def get_dist2(self):
+        return self.dist.read_all()
+
+    def ping_dist(self):
+        while True:
+            self.dist.write(b'P')
+            print("ping it")
+            distance = self.dist.read_all()
+            dist = str(distance).strip().split("\'")[1]
+            print(dist)
+            if dist:
+                dist = dist.split("\\")[0]
+                return int(dist)
+            sleep(0.2)
+
+    def ping_get_all(self):
+        distance = self.dist.read_all()
+        print(distance)
+
     def change2processing(self):
         self.light.write(b'P')
         print("write P")
         self.__processing_flag = True
         self.__working_flag = False
+
+    def ping2processing(self):
+        for i in range(4):
+            self.light.write(b'P')
+            print("ping it")
+            # resonse = self.dist.read()
+            # res = str(resonse).strip().split("\'")[1]
+            # print(resonse)
+            sleep(0.2)
+            # if dist:
+            #     dist = dist.split("\\")[0]
+            #     return dist
+
 
     def change2working(self):
         self.light.write(b'W')
@@ -47,7 +79,24 @@ class arduinoConnection(object):
         self.__working_flag = True
         self.__processing_flag = False
 
+adConn = arduinoConnection()
+# adConn.read_dist()
+# print("the dist is " + str(adConn.ping_dist()))
+adConn.ping2processing()
 
+    # print(d)
+    # if 6 < d < 30:
+    #     if not adConn.__working_flag:
+    #         adConn.change2working()
+    # elif d <= 6:
+    #     if not adConn.__processing_flag:
+    #         adConn.change2processing()
+
+
+
+# while True:
+#     adConn.ping_get_all()
+#     sleep(5)
 
 
 
---
+++ distance_sensor/ping/ping.ino
@@ -0,0 +1,51 @@
+#define trigPin 13
+#define echoPin 12
+#define led 10
+
+int flag;
+boolean command = false;
+
+void setup() {
+  Serial.begin (9600);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  if (command) {
+  	if(flag == 'P') {
+
+  		ping();
+  		command = false;
+  		flag = 0;
+  	}
+  }
+  delay(1000);
+}
+
+void serialEvent() {
+  while (Serial.available()) {
+    // get the new byte:
+    flag = Serial.read();
+    command = true;
+    // Serial.println(flag);
+    digitalWrite(led,HIGH);
+
+    // }
+  }
+}
+
+void ping() {
+  long duration, dist_relay, dist_led, dist;
+  digitalWrite(trigPin, LOW);  // Added this line
+  delayMicroseconds(2); // Added this line
+  digitalWrite(trigPin, HIGH);
+  //  delayMicroseconds(1000); - Removed this line
+  delayMicroseconds(10); // Added this line
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  dist = (duration/2) / 29.1;
+  Serial.println(dist);
+  digitalWrite(led,LOW);
+}
